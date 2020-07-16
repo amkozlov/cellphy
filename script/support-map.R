21fece418cec3525b12db58f5f09c76b4d7e7224
Alexey Kozlov <alexey.kozlov@h-its.org>
2020-07-16 13:59:54 +0200

    add script for support tree vizualization

---
+++ script/support-map.R
@@ -0,0 +1,55 @@
+#!/usr/bin/env Rscript
+# CellPhy - Bootstrap support plot
+# Created by: Alexey Kozlov, Joao M Alves, Alexandros Stamatakis & David Posada - 15.07.2020
+# mailto: jalves@uvigo.es
+
+# Load required libraries
+
+if (!suppressMessages(require('ggtree', quietly = T))) { 
+  message("ERROR: package not found: ggtree\nPlease run install.sh to fix.")
+  quit()
+}
+
+usage <- function() 
+{ 
+  message ("CellPhy - Mutation mapping plot - 15.07.2020")
+  message ("Created by: Alexey Kozlov, Joao M Alves, Alexandros Stamatakis & David Posada\n")
+  message ("Usage: ./support-map.R raxml.support [OutputPrefix]\n")
+}
+
+#Read arguments:
+args = commandArgs(trailingOnly=TRUE)
+if (length(args) >= 1) {
+  treef = args[1]
+  if (length(args) >= 2) {
+    prefix <- args[2]
+  } else {
+    prefix <- treef
+  }
+} else {
+  usage()
+  quit()
+}
+
+#Load Tree
+tree <- read.tree(treef)
+
+#Plot and save
+out_pdf = paste(prefix, ".pdf", sep="")
+out_svg = paste(prefix, ".svg", sep="")
+
+p = ggtree(tree, branch.length='none') + geom_tiplab(size=1.8) 
+
+p = p + geom_nodelab(size=3, hjust=1.7, vjust=-1.0)
+
+pdf(out_pdf)
+
+p
+
+svg(out_svg)
+
+p
+
+suppressMessages(dev.off())
+
+
